3cf184ecebc9d5615d5f243bc0545aa2393cc859
Furffico <jerrywang0206@gmail.com>
2024-04-06 12:14:21 +0800

    [plus] add unique loss (v2)

--- deepaco_plus/model/model.py
+++ deepaco_plus/model/model.py
@@ -1,8 +1,11 @@
-from typing import Optional, Union
+from typing import Any, Optional, Union
+
+import torch.nn as nn
 
 from rl4co.envs.common.base import RL4COEnvBase
 from rl4co.models.rl import REINFORCE
 from rl4co.models.rl.reinforce.baselines import REINFORCEBaseline
+from rl4co.utils.utils import merge_with_defaults
 
 from .policy import DeepACOPlusPolicy
 
@@ -23,6 +26,7 @@ class DeepACOPlus(REINFORCE):
         env: RL4COEnvBase,
         policy: Optional[DeepACOPlusPolicy] = None,
         baseline: Union[REINFORCEBaseline, str] = "exponential",
+        train_kwargs: Optional[dict] = None,
         policy_kwargs: dict = {},
         baseline_kwargs: dict = {},
         **kwargs,
@@ -30,4 +34,46 @@ class DeepACOPlus(REINFORCE):
         if policy is None:
             policy = DeepACOPlusPolicy(env.name, **policy_kwargs)
 
+        self.train_kwargs = merge_with_defaults(train_kwargs, gamma=1.0)
+
         super().__init__(env, policy, baseline, baseline_kwargs, **kwargs)
+
+    def shared_step(
+        self, batch: Any, batch_idx: int, phase: str, dataloader_idx: int = None
+    ):
+        td = self.env.reset(batch)
+        # Perform forward pass (i.e., constructing solution and computing log-likelihoods)
+        out = self.policy(td, self.env, phase=phase)
+
+        # Compute loss
+        if phase == "train":
+            out = self.calculate_loss(td, batch, out)
+            reward_loss = out.get("loss", None)
+            unique_loss = self.unique_loss(td, batch, out)
+            loss = reward_loss + self.train_kwargs["gamma"] * unique_loss
+
+            out.update(
+                dict(
+                    loss_rw=reward_loss,
+                    loss_u=unique_loss,
+                    loss=loss,
+                )
+            )
+            metrics = self.log_metrics(out, phase, dataloader_idx=dataloader_idx)
+            return {
+                "loss_rw": reward_loss,
+                "loss_u": unique_loss,
+                "loss": loss,
+                **metrics,
+            }
+        else:
+            metrics = self.log_metrics(out, phase, dataloader_idx=dataloader_idx)
+            return {"loss": None, "loss_rw": None, "loss_u": None, **metrics}
+
+    @staticmethod
+    def unique_loss(td, batch, out):
+        heatmap_logp = out["heatmap"]
+        heatmap = nn.functional.softmax(heatmap_logp, -1)
+        colsum = heatmap.sum(-2)
+        loss_u = ((colsum - 1) ** 2).sum(-1).mean()
+        return loss_u
--- deepaco_plus/model/policy.py
+++ deepaco_plus/model/policy.py
@@ -2,10 +2,17 @@ from typing import Optional, Union
 
 import torch.nn as nn
 
+from tensordict import TensorDict
+
+from rl4co.envs import get_env
 from rl4co.envs.common.base import RL4COEnvBase
+from rl4co.models.nn.utils import get_log_likelihood
 from rl4co.models.zoo.common.nonautoregressive.policy import NonAutoregressivePolicy
+from rl4co.utils.pylogger import get_pylogger
 
 from .decoder import DeepACOPlusDecoder
+
+log = get_pylogger(__name__)
 
 
 class DeepACOPlusPolicy(NonAutoregressivePolicy):
@@ -59,3 +66,68 @@ class DeepACOPlusPolicy(NonAutoregressivePolicy):
             val_decode_type="multistart_sampling",
             test_decode_type="multistart_sampling",
         )
+
+    def forward(
+        self,
+        td: TensorDict,
+        env: Union[str, RL4COEnvBase, None] = None,
+        phase: str = "train",
+        return_actions: bool = False,
+        return_entropy: bool = False,
+        return_init_embeds: bool = False,
+        **decoder_kwargs,
+    ) -> dict:
+        """Forward pass of the policy.
+
+        Args:
+            td: TensorDict containing the environment state
+            env: Environment to use for decoding
+            phase: Phase of the algorithm (train, val, test)
+            return_actions: Whether to return the actions
+            return_entropy: Whether to return the entropy
+            return_init_embeds: Whether to return the initial embeddings
+            decoder_kwargs: Keyword arguments for the decoder
+
+        Returns:
+            out: Dictionary containing the reward, log likelihood, and optionally the actions and entropy
+        """
+
+        # ENCODER: get embeddings from initial state
+        graph, init_embeds = self.encoder(td)
+
+        # Instantiate environment if needed
+        if isinstance(env, str) or env is None:
+            env_name = self.env_name if env is None else env
+            log.info(f"Instantiated environment not provided; instantiating {env_name}")
+            env = get_env(env_name)
+
+        # Get decode type depending on phase
+        if decoder_kwargs.get("decode_type", None) is None:
+            decoder_kwargs["decode_type"] = getattr(self, f"{phase}_decode_type")
+
+        # DECODER: main rollout with autoregressive decoding
+        log_p, actions, td_out = self.decoder(
+            td, graph, env, phase=phase, **decoder_kwargs
+        )
+
+        out = {k: td_out[k] for k in ("reward", "heatmap")}
+
+        if phase == "train":
+            # Log likelihood is calculated within the model
+            log_likelihood = get_log_likelihood(
+                log_p, actions, td_out.get("mask", None)
+            )  # , return_sum=False).mean(-1)
+            out["log_likelihood"] = log_likelihood
+
+        if return_actions:
+            out["actions"] = actions
+
+        if return_entropy:
+            entropy = -(log_p.exp() * log_p).nansum(dim=1)  # [batch, decoder steps]
+            entropy = entropy.sum(dim=1)  # [batch]
+            out["entropy"] = entropy
+
+        if return_init_embeds:
+            out["init_embeds"] = init_embeds
+
+        return out
